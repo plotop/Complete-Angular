6d35041d54997be1f3810d2f29f8c489d83a9ce3
Sairam Ghanta <vyshuram.tanya@outlook.com>
2018-04-10 13:47:05 +0200

    teo-way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
  allowNewServer = false;
  serverCreationStatus = 'No Server was Created!';
- serverName = '';
+ serverName = 'TestServer';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
